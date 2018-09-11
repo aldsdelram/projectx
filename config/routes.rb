701726caded4af27bb8be7b28bc8849053f5a936
Aldrin Ramos <aldrin@im.skubbs.com>
2018-09-11 14:21:07 +0800

    SETUP: Devise routing, redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  get 'admin/index'
+	devise_for :users, controllers: {
+		sessions: 'user/sessions',
+		# registrations: 'users/registrations'
+	}
+
+	devise_scope :user do
+	  # authenticated :user do
+	  #   root '/', as: :authenticated_root
+	  # end
+
+	  unauthenticated do
+	    root 'devise/sessions#new', as: :unauthenticated_root
+	  end
+	  
+	  get '/signout', to: 'devise/sessions#destroy', as: :signout
+	end
+
   mount RailsAdmin::Engine => '/command_center/super_admin', as: 'rails_admin'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
